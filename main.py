8e260a89022b806ea6c37a6435b5a04495c076a7
tasnimtabbi <tabbianneni.tasnim@gmail.com>
2025-12-02 16:20:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from datetime import date, datetime
+
+# Obtenir la date d'aujourd'hui
+aujourdhui = date.today()
+heure_actuelle = datetime.now().strftime("%H:%M:%S")
+print(aujourdhui, heure_actuelle) 
+
 
